c13b79c9ef580740a14e9fad8fafb00bd27d7510
Jason Green <jasoniangreen@gmail.com>
2016-07-15 14:31:54 +0100

    Trigger travis

--- test/processors.spec.js
+++ test/processors.spec.js
@@ -4,7 +4,6 @@ var IsoForm = require('../lib/iso-form'),
     isoFormTestDom = require('./processors/test-dom'),
     should = require('chai').should();
 
-
 describe('IsoForm processors', function () {
     it('should accept processor and bootstrap types', function () {
         var isoForm = new IsoForm(isoFormTestDom);
